534ebea875adbce56fc46c05bb89c08f8a9c348f
tapanpatra123 <patratapan2@gmail.com>
2020-07-25 11:53:41 +0530

    cc

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from "react";
+
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+        heloo
+      </div>
+    );
+  }
+}
